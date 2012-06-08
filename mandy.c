71bf86813756ba785af51763877f08c7c3195c75
litao wang <litaohill@163.com>
2012-06-08 14:07:40 +0800

    modified by litaowang0608-14.04

--- mandy.c
+++ mandy.c
@@ -1,2 +1,12 @@
 
-#include<>
+
+#include<stdio.h>
+int main()
+{
+ printf("This is just a github-test! add by litaowang201206081404!\n");
+ printf("modified and remote update by litaowang 201206081404\n");
+
+ return 0;
+
+
+}
